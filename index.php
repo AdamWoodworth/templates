69c0ff32456cb955ab984c960997ae611867b306
Adam (from home) <adamrwoodworth@gmail.com>
2017-05-12 05:24:09 -0700

    Menu Reworking

--- index.php
+++ index.php
@@ -41,10 +41,10 @@
 </script>
 <!-- -END- Mobile Navigation Function -->
 
-<!-- Adobe Typekit Code -->
+<!-- Begin Adobe Typekit -->
 <script src="https://use.typekit.net/qwp8tzf.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
-<!-- Adobe Typekit Code End -->
+<!-- -End- Adobe Typekit -->
 
 <!-- Begin WP Head Function -->
 <?php wp_head(); ?>
@@ -56,52 +56,59 @@
 <!-- Begin Header -->
 <div id="header">
 	<h1 id="logo"><a href="main.html"><img src="<?php bloginfo('template_directory'); ?>/images/logo.svg"></a></h1>
-	<img id="toggle" src="<?php bloginfo('template_directory'); ?>/images/toggle.svg" width="25" height="25" alt="Toggle Menu">
 </div>
 <!-- -END- Header -->
 
-<!-- Begin NAVIGATION -->
+<!-- Begin Navigation -->
 <!-- Begin Wordpress Menu -->
+
+<img id="toggle" src="<?php bloginfo('template_directory'); ?>/images/toggle.svg" width="25" height="25" alt="Toggle Menu">
+
 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'container' => 'div', 'container_class' => 'navigation')); ?>
+
 <!-- -END- Wordpress Menu -->
 <!-- -END- Navigation -->
 
 <!-- Begin Content -->
-<!-- Begin Text -->
-	<div id="content" class="page">
-		
-		<?php if (have_posts()) : while (have_posts()) : The_post(); ?>
-		<article id="article-<?php the_ID(); ?>" class="article">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2> <!-- Pulls and crams the Title with parmalink attached-->
-		<?php the_content(); ?> <!-- Pulls and crams the article -->
-		<small>index.php</small> <!-- Remove eventually -->
-		</article>
-		<?php endwhile; endif; ?>
-		
-	</div>
-<!-- -END- Text -->
+<div id="content" class="page">
+
+	<?php if (have_posts()) : while (have_posts()) : The_post(); ?>
+	<article id="article-<?php the_ID(); ?>" class="article">
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2> <!-- Pulls and crams the Title with parmalink attached-->
+	<?php the_content(); ?> <!-- Pulls and crams the article -->
+	<small>index.php</small> <!-- Remove eventually -->
+	</article>
+	<?php endwhile; endif; ?>
+
+</div>
 <!-- -END- Content -->
 
 <!-- Begin Footer -->
 <footer>
-  <div class="makersmark"><a href="http://incandescentideas.com" target="_blank"><img id="aw-logo" src="<?php bloginfo('template_directory'); ?>/images/aw_logo.svg" alt="Shameless Self Promotion"/></a> 
+  <div class="makersmark">
+	  <a href="http://incandescentideas.com" target="_blank"><img id="aw-logo" src="<?php bloginfo('template_directory'); ?>/images/aw_logo.svg" alt="Shameless Self Promotion"/></a> 
   </div> 
   <div class="footer_column">
     <div class="social">
       <div class="square">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_facebook</title><path d="M0,500V0H500V500H0Zm345,0V306.38h65l9.73-75.46H345V182.74c0-21.85,6.07-36.74,37.4-36.74l40,0V78.49c-6.91-.92-30.63-3-58.23-3-57.61,0-97.05,35.17-97.05,99.75v55.65H201.91v75.46h65.16V500H345Z" style="fill:#fff"/></svg>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_facebook</title>
+        <path d="M0,500V0H500V500H0Zm345,0V306.38h65l9.73-75.46H345V182.74c0-21.85,6.07-36.74,37.4-36.74l40,0V78.49c-6.91-.92-30.63-3-58.23-3-57.61,0-97.05,35.17-97.05,99.75v55.65H201.91v75.46h65.16V500H345Z" style="fill:#fff"/></svg>
       </div>
       <div class="square">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_instagram</title><path d="M0,0V500H500L499.54,0.45ZM250,176.73A73.27,73.27,0,1,1,176.73,250,73.37,73.37,0,0,1,250,176.73h0ZM441.86,410.29c0,17.4-16.22,31.57-33.62,31.57H93c-17.44,0-34.84-14.17-34.84-31.57V252.67c0-17.44,17.4-31.74,34.84-31.74h17.58A11.51,11.51,0,0,1,122,234a125.11,125.11,0,0,0-1.09,16c0,71.17,57.92,129.08,129.09,129.08S379.09,321.18,379.09,250A125.11,125.11,0,0,0,378,234a11.72,11.72,0,0,1,2.77-9.13,11.59,11.59,0,0,1,8.67-3.91h18.8c17.44,0,33.62,14.3,33.62,31.74V410.29Zm0-290.65h0a31.54,31.54,0,0,1-31.52,31.52h-30a31.54,31.54,0,0,1-31.52-31.52v-30a31.54,31.54,0,0,1,31.52-31.52h30a31.54,31.54,0,0,1,31.52,31.52v30Zm0,0" style="fill:#fff"/></svg>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_instagram</title>
+        <path d="M0,0V500H500L499.54,0.45ZM250,176.73A73.27,73.27,0,1,1,176.73,250,73.37,73.37,0,0,1,250,176.73h0ZM441.86,410.29c0,17.4-16.22,31.57-33.62,31.57H93c-17.44,0-34.84-14.17-34.84-31.57V252.67c0-17.44,17.4-31.74,34.84-31.74h17.58A11.51,11.51,0,0,1,122,234a125.11,125.11,0,0,0-1.09,16c0,71.17,57.92,129.08,129.09,129.08S379.09,321.18,379.09,250A125.11,125.11,0,0,0,378,234a11.72,11.72,0,0,1,2.77-9.13,11.59,11.59,0,0,1,8.67-3.91h18.8c17.44,0,33.62,14.3,33.62,31.74V410.29Zm0-290.65h0a31.54,31.54,0,0,1-31.52,31.52h-30a31.54,31.54,0,0,1-31.52-31.52v-30a31.54,31.54,0,0,1,31.52-31.52h30a31.54,31.54,0,0,1,31.52,31.52v30Zm0,0" style="fill:#fff"/></svg>
       </div>
       <div class="square">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_pinterest</title><path d="M0,0V500H500V0H0ZM279.79,358.85c-24.12,0-46.78-13-54.58-27.85,0,0-13,51.49-15.74,61.44-4.72,17.18-51.29,102.06-51.29,102.06s2.33-98.23,6.35-115.14c4.27-18.16,28.73-121.8,28.73-121.8s-7.14-14.26-7.14-35.34c0-33.12,19.19-57.86,43.08-57.86,20.35,0,30.14,15.26,30.14,33.55,0,20.44-13,51-19.71,79.33-5.58,23.69,11.91,43,35.27,43,42.36,0,70.87-54.39,70.87-118.82,0-49-33-85.66-93-85.66-67.81,0-110,50.57-110,107,0,19.46,5.75,33.18,14.74,43.81,4.14,4.89,4.71,6.87,3.21,12.47-1.07,4.12-3.54,14-4.56,17.93-1.48,5.66-6.07,7.69-11.19,5.59-31.21-12.78-45.76-46.94-45.76-85.39,0-63.5,53.56-139.61,159.75-139.61,85.29,0,141.46,61.77,141.46,128C400.39,293.36,351.66,358.85,279.79,358.85Z" style="fill:#f7f8f8"/></svg>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_pinterest</title>
+        <path d="M0,0V500H500V0H0ZM279.79,358.85c-24.12,0-46.78-13-54.58-27.85,0,0-13,51.49-15.74,61.44-4.72,17.18-51.29,102.06-51.29,102.06s2.33-98.23,6.35-115.14c4.27-18.16,28.73-121.8,28.73-121.8s-7.14-14.26-7.14-35.34c0-33.12,19.19-57.86,43.08-57.86,20.35,0,30.14,15.26,30.14,33.55,0,20.44-13,51-19.71,79.33-5.58,23.69,11.91,43,35.27,43,42.36,0,70.87-54.39,70.87-118.82,0-49-33-85.66-93-85.66-67.81,0-110,50.57-110,107,0,19.46,5.75,33.18,14.74,43.81,4.14,4.89,4.71,6.87,3.21,12.47-1.07,4.12-3.54,14-4.56,17.93-1.48,5.66-6.07,7.69-11.19,5.59-31.21-12.78-45.76-46.94-45.76-85.39,0-63.5,53.56-139.61,159.75-139.61,85.29,0,141.46,61.77,141.46,128C400.39,293.36,351.66,358.85,279.79,358.85Z" style="fill:#f7f8f8"/></svg>
       </div>
       <div class="square">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_tumblr</title><path d="M0,0V500H500V0H0ZM177.13,338.82l0-119.69H132.52V163.22c33.74-9.61,62.86-45.46,73.56-83.48C210.21,63.2,210.65,52,210.65,52h63.43v102h74.21v64.34H273.67v37.24c0,25-.08,50,0,75,0,66.27,63.65,39.06,69.46,37.78l18.32,60.52C345.63,445.64,177.13,486.61,177.13,338.82Z" style="fill:#fff"/></svg>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_tumblr</title>
+        <path d="M0,0V500H500V0H0ZM177.13,338.82l0-119.69H132.52V163.22c33.74-9.61,62.86-45.46,73.56-83.48C210.21,63.2,210.65,52,210.65,52h63.43v102h74.21v64.34H273.67v37.24c0,25-.08,50,0,75,0,66.27,63.65,39.06,69.46,37.78l18.32,60.52C345.63,445.64,177.13,486.61,177.13,338.82Z" style="fill:#fff"/></svg>
       </div>
       <div class="square">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_twitter</title><path d="M386.12,178.11q0.2,4.45.2,9c0,91.51-69.65,197-197,197A196,196,0,0,1,83.14,353a141,141,0,0,0,16.52,1,139,139,0,0,0,86-29.64A69.32,69.32,0,0,1,121,276.22,69.42,69.42,0,0,0,152.25,275a69.29,69.29,0,0,1-55.55-67.9c0-.29,0-0.58,0-0.87a69,69,0,0,0,31.37,8.66,69.33,69.33,0,0,1-21.43-92.45,196.6,196.6,0,0,0,142.73,72.35,69.29,69.29,0,0,1,118-63.16,138.65,138.65,0,0,0,44-16.81,69.45,69.45,0,0,1-30.45,38.31,138.43,138.43,0,0,0,39.77-10.9A140.74,140.74,0,0,1,386.12,178.11ZM500,0H0V500H500V0Z" style="fill:#fff"/></svg>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><title>logo_twitter</title>
+        <path d="M386.12,178.11q0.2,4.45.2,9c0,91.51-69.65,197-197,197A196,196,0,0,1,83.14,353a141,141,0,0,0,16.52,1,139,139,0,0,0,86-29.64A69.32,69.32,0,0,1,121,276.22,69.42,69.42,0,0,0,152.25,275a69.29,69.29,0,0,1-55.55-67.9c0-.29,0-0.58,0-0.87a69,69,0,0,0,31.37,8.66,69.33,69.33,0,0,1-21.43-92.45,196.6,196.6,0,0,0,142.73,72.35,69.29,69.29,0,0,1,118-63.16,138.65,138.65,0,0,0,44-16.81,69.45,69.45,0,0,1-30.45,38.31,138.43,138.43,0,0,0,39.77-10.9A140.74,140.74,0,0,1,386.12,178.11ZM500,0H0V500H500V0Z" style="fill:#fff"/></svg>
       </div>
     </div>
   </div>
